cf63c5a3db0242d182acd66c8057cec3828ecb04
mikhail <mikhail@pop-os.localdomain>
2021-07-27 11:02:06 +0300

    Версия 27072021

--- conftest.py
+++ conftest.py
@@ -5,7 +5,7 @@ from selenium import webdriver
 
 def pytest_addoption(parser):
     parser.addoption("--browser", action="store", default="chrome")
-    parser.addoption("--executor", action="store", default="192.168.1.73")
+    parser.addoption("--mobile", action="store_true", help="test mobile emulation")
     parser.addoption("--prod", action="store", default="http://demo.opencart.com")
     parser.addoption("--stage", action="store", choices=["branch", "staging"], default="branch")
 
@@ -13,29 +13,27 @@ def pytest_addoption(parser):
 @pytest.fixture(scope="session")
 def browser(request):
     browser = request.config.getoption("--browser")
-    executor = request.config.getoption("--executor")
+    mobile = request.config.getoption("--mobile")
     prod_url = request.config.getoption("--prod")
     stag_url = "https://konflic.github.io/front_example/{}.html".format(request.config.getoption("--stage"))
 
-    executor_url = f"http://{executor}:4444/wd/hub"
-
     caps = {
         "browserName": browser,
+        "browserVersion": "91.0" if browser == "chrome" else "",
         "screenResolution": "1920x1080",
         "name": "Mikhail",
-        'acceptSslCerts': True,
         "selenoid:options": {
             "enableVNC": True
         },
-        'acceptInsecureCerts': True,
         'timeZone': 'Europe/Moscow',
-        'goog:chromeOptions': {
-            'args': []
-        }
+        'goog:chromeOptions': {}
     }
 
+    if browser == "chrome" and mobile:
+        caps["goog:chromeOptions"]["mobileEmulation"] = {"deviceName": "iPhone 5/SE"}
+
     driver = webdriver.Remote(
-        command_executor=executor_url,
+        command_executor="http://192.168.1.79:4444/wd/hub",
         desired_capabilities=caps
     )
 
